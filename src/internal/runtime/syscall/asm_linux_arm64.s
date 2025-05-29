811c997b591020ba5248e64882e3a65759d135c6
Aleksey Markin <alexanius@gmail.com>
2025-05-29 13:27:43 +0300

    Update asm_linux_arm64.s

--- src/internal/runtime/syscall/asm_linux_arm64.s
+++ src/internal/runtime/syscall/asm_linux_arm64.s
@@ -4,26 +4,18 @@
 
 #include "textflag.h"
 
-// func Syscall6(num, a1, a2, a3, a4, a5, a6 uintptr) (r1, r2, errno uintptr)
-TEXT ·Syscall6(SB),NOSPLIT,$0-80
-	MOVD	num+0(FP), R8	// syscall entry
-	MOVD	a1+8(FP), R0
-	MOVD	a2+16(FP), R1
-	MOVD	a3+24(FP), R2
-	MOVD	a4+32(FP), R3
-	MOVD	a5+40(FP), R4
-	MOVD	a6+48(FP), R5
-	SVC
-	CMN	$4095, R0
-	BCC	ok
-	MOVD	$-1, R4
-	MOVD	R4, r1+56(FP)
-	MOVD	ZR, r2+64(FP)
-	NEG	R0, R0
-	MOVD	R0, errno+72(FP)
-	RET
-ok:
-	MOVD	R0, r1+56(FP)
-	MOVD	R1, r2+64(FP)
-	MOVD	ZR, errno+72(FP)
-	RET
+// func Syscall6(a1, a2, a3, a4, a5, a6, num uintptr) (r1, r2, errno uintptr)
+TEXT ·Syscall6<ABIInternal>(SB),NOSPLIT,$0-0
+        MOVD    R0, R8
+        MOVD    R1, R0
+        MOVD    R2, R1
+        MOVD    R3, R2
+        MOVD    R4, R3
+        MOVD    R5, R4
+        MOVD    R6, R5
+        SVC
+        CMN     $4095, R0
+        CSNEG   CC, ZR, R0, R2 // if CC then R2 = ZR else R2 = -R0
+        CSINV   CC, R0, ZR, R0 // if CC then R0 = R0 else R0 = ZR
+        CSET    CC, R1         // if CC then R1 = 1 else R1 = 0
+        RET
